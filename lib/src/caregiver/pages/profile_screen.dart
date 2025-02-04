73330d0a8b3d1bad9c28d05f898a0aea98c4119b
Hollowpath <106108688+Hollowpath@users.noreply.github.com>
2025-02-04 18:42:03 +0800

    Your commit message

--- lib/src/caregiver/pages/profile_screen.dart
+++ lib/src/caregiver/pages/profile_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:io';
-import 'dart:async'; // <-- Add this import for Completer
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
